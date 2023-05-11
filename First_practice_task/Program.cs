c01b0259d039f8f78e13e4282b3c83a1e7c7ac9f
Liudmila Romanovskaia <ludmila170500@gmail.com>
2023-05-11 21:52:22 +0400

    Initial commit

---
+++ First_practice_task/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace First_practice_task
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            string sequenceString = Console.ReadLine();
+            if (sequenceString.Length == 0)
+            {
+                Console.WriteLine("Ошибка: строка не указана в аргументах командной строки.");
+                return;
+            }
+
+            int maxLength = 0;
+            int currentLength = 0;
+            char previousChar = '\0';
+
+            foreach (char c in sequenceString)
+            {
+                if (c != previousChar)
+                {
+                    currentLength++;
+                    if (currentLength > maxLength) maxLength = currentLength;
+                }
+                else currentLength = 1;
+
+                previousChar = c;
+            }
+
+            Console.WriteLine("Максимальное количество неодинаковых последовательных символов в строке: " + maxLength);
+        }
+    }
+}
